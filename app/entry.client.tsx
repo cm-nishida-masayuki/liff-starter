aa1ee1d62bc5d8983f4497b54155943531758c46
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2024-03-19 16:09:32 +0900

    fix

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,7 +7,7 @@ import OneSignal from "react-onesignal";
 liff.init({ liffId: import.meta.env.VITE_LIFF_ID }).then(() => {
   if (window.matchMedia("(display-mode: standalone)").matches) {
     /* @ts-ignore */
-    Weglot.initialize({ api_key: "wg_f288cd21cd31d84fe6698af0cc15d0810" });
+    Weglot.initialize({ api_key: "wg_67bc3f46b6cc4ef291697b629756766c0" });
     OneSignal.init({ appId: "a8c5a8f8-d638-47b0-90c2-3b30b8fe85bd" }).then(
       () => {
         OneSignal.Slidedown.promptPush();
