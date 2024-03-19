f4380856a648ce98ed1989502b734487314d1ac0
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2024-03-19 14:55:18 +0900

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -6,13 +6,23 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+const initWeglot = () => {
+  /* @ts-ignore */
+  Weglot.initialize({ api_key: "wg_f288cd21cd31d84fe6698af0cc15d0810" });
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
+        <script
+          type="text/javascript"
+          src="https://cdn.weglot.com/weglot.min.js"
+        ></script>
+        <script>initWeglot()</script>
         <Meta />
         <Links />
       </head>
